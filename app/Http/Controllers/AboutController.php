63f9c0177aec8b9cd4e93e7b083eb1bc0b52a524
askholani <askholani210@gmail.com>
2023-02-22 09:27:57 +0700

    praktikum 2 pertemuan 2

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function about () {
-        return 'Ibnu Hajar Askholani, 2141720170';
+        return 'Ibnu Hajar Askholani, 2141720170 a';
     }
 }
